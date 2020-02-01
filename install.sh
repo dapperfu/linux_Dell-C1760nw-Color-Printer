8092b4700a7484919de7a02eab2870166ad2953d
Jed <jed-frey@users.noreply.github.com>
2020-02-01 15:47:54 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+wget https://github.com/jed-frey/linux_Dell-C1760nw-Color-Printer/releases/download/v0.0.1/6000_6010_deb_1.01_20110210.zip
+unzip 6000_6010_deb_1.01_20110210.zip
+
+dpkg --add-architecture i386
+dpkg -i deb_1.01_20110210/xerox-phaser-6000-6010_1.0-1_i386.deb
+apt-get install -f
+apt-cache search libcups | cut -f1 -d" " | xargs -n1 -I{} apt-get install --yes {}:i386
