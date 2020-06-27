ef67f3a6f16435a476fc847c30c8e6b9e459873e
- js <223258+dapperfu@users.noreply.github.com>
2020-06-27 11:53:12 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ unzip 6000_6010_deb_1.01_20110210.zip
 
 dpkg --add-architecture i386
 dpkg -i deb_1.01_20110210/xerox-phaser-6000-6010_1.0-1_i386.deb
-apt-get install -f
+apt-get install -f --yes
 apt-cache search libcups | cut -f1 -d" " | xargs -n1 -I{} apt-get install --yes {}:i386
